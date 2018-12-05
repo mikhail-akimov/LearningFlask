4bb4a410fe759906bd0a07b1e14baa9fc44be550
makimov <daggerito@gmail.com>
2018-12-05 17:28:00 +0300

    Learning Flask tutorial ch.6 (Profile + Edit)

--- microblog.py
+++ microblog.py
@@ -1,7 +1,16 @@
 from app import app, db
 from app.models import User, Post
+from flask_login import current_user
+from datetime import datetime
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
